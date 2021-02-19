466d8ce3fff45c5811a88f715480562568ddbc70
Federico Dessì <dessi.fed@gmail.com>
2021-02-19 19:23:58 +0100

    Add project files.

---
+++ logAnalizer/logAnalizer.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace logAnalizer
+{
+    partial class logAnalizer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.logResultText = new System.Windows.Forms.TextBox();
+            this.logSelectionLbl = new System.Windows.Forms.Label();
+            this.logSelectionBtn = new System.Windows.Forms.Button();
+            this.generateMetricsBtn = new System.Windows.Forms.Button();
+            this.numNodes = new System.Windows.Forms.TextBox();
+            this.numPackets = new System.Windows.Forms.TextBox();
+            this.numNodesLbl = new System.Windows.Forms.Label();
+            this.numPacketsLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // logResultText
+            // 
+            this.logResultText.Location = new System.Drawing.Point(10, 177);
+            this.logResultText.Multiline = true;
+            this.logResultText.Name = "logResultText";
+            this.logResultText.ReadOnly = true;
+            this.logResultText.Size = new System.Drawing.Size(780, 268);
+            this.logResultText.TabIndex = 0;
+            // 
+            // logSelectionLbl
+            // 
+            this.logSelectionLbl.AutoSize = true;
+            this.logSelectionLbl.Location = new System.Drawing.Point(185, 63);
+            this.logSelectionLbl.Name = "logSelectionLbl";
+            this.logSelectionLbl.Size = new System.Drawing.Size(124, 13);
+            this.logSelectionLbl.TabIndex = 1;
+            this.logSelectionLbl.Text = "Nessun file selezionato...";
+            // 
+            // logSelectionBtn
+            // 
+            this.logSelectionBtn.Location = new System.Drawing.Point(12, 58);
+            this.logSelectionBtn.Name = "logSelectionBtn";
+            this.logSelectionBtn.Size = new System.Drawing.Size(155, 23);
+            this.logSelectionBtn.TabIndex = 2;
+            this.logSelectionBtn.Text = "Seleziona log del sink";
+            this.logSelectionBtn.UseVisualStyleBackColor = true;
+            this.logSelectionBtn.Click += new System.EventHandler(this.logSelectionBtn_Click);
+            // 
+            // generateMetricsBtn
+            // 
+            this.generateMetricsBtn.BackColor = System.Drawing.Color.LimeGreen;
+            this.generateMetricsBtn.FlatAppearance.BorderSize = 0;
+            this.generateMetricsBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.generateMetricsBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generateMetricsBtn.ForeColor = System.Drawing.Color.White;
+            this.generateMetricsBtn.Location = new System.Drawing.Point(260, 95);
+            this.generateMetricsBtn.Name = "generateMetricsBtn";
+            this.generateMetricsBtn.Size = new System.Drawing.Size(265, 76);
+            this.generateMetricsBtn.TabIndex = 3;
+            this.generateMetricsBtn.Text = "Genera Statistiche";
+            this.generateMetricsBtn.UseVisualStyleBackColor = false;
+            this.generateMetricsBtn.Click += new System.EventHandler(this.generateMetricsBtn_Click);
+            // 
+            // numNodes
+            // 
+            this.numNodes.Location = new System.Drawing.Point(655, 29);
+            this.numNodes.Name = "numNodes";
+            this.numNodes.Size = new System.Drawing.Size(95, 20);
+            this.numNodes.TabIndex = 4;
+            // 
+            // numPackets
+            // 
+            this.numPackets.Location = new System.Drawing.Point(655, 63);
+            this.numPackets.Name = "numPackets";
+            this.numPackets.Size = new System.Drawing.Size(95, 20);
+            this.numPackets.TabIndex = 5;
+            // 
+            // numNodesLbl
+            // 
+            this.numNodesLbl.AutoSize = true;
+            this.numNodesLbl.Location = new System.Drawing.Point(569, 32);
+            this.numNodesLbl.Name = "numNodesLbl";
+            this.numNodesLbl.Size = new System.Drawing.Size(81, 13);
+            this.numNodesLbl.TabIndex = 6;
+            this.numNodesLbl.Text = "Numero di nodi:";
+            // 
+            // numPacketsLbl
+            // 
+            this.numPacketsLbl.AutoSize = true;
+            this.numPacketsLbl.Location = new System.Drawing.Point(535, 66);
+            this.numPacketsLbl.Name = "numPacketsLbl";
+            this.numPacketsLbl.Size = new System.Drawing.Size(117, 13);
+            this.numPacketsLbl.TabIndex = 7;
+            this.numPacketsLbl.Text = "Totale pacchetti inviati:";
+            // 
+            // logAnalizer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numPacketsLbl);
+            this.Controls.Add(this.numNodesLbl);
+            this.Controls.Add(this.numPackets);
+            this.Controls.Add(this.numNodes);
+            this.Controls.Add(this.generateMetricsBtn);
+            this.Controls.Add(this.logSelectionBtn);
+            this.Controls.Add(this.logSelectionLbl);
+            this.Controls.Add(this.logResultText);
+            this.Name = "logAnalizer";
+            this.Text = "Log Analizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox logResultText;
+        private System.Windows.Forms.Label logSelectionLbl;
+        private System.Windows.Forms.Button logSelectionBtn;
+        private System.Windows.Forms.Button generateMetricsBtn;
+        private System.Windows.Forms.TextBox numNodes;
+        private System.Windows.Forms.TextBox numPackets;
+        private System.Windows.Forms.Label numNodesLbl;
+        private System.Windows.Forms.Label numPacketsLbl;
+    }
+}
+
---
+++ logAnalizer/logAnalizer.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace logAnalizer
+{
+    public partial class logAnalizer : Form
+    {
+       
+        public logAnalizer()
+        {
+            InitializeComponent();
+        }
+
+        private void generateMetricsBtn_Click(object sender, EventArgs e)
+        {
+            if(File.Exists(logSelectionLbl.Text) && numNodes.Text != null && !numNodes.Text.Equals("") && numPackets.Text != null && !numPackets.Text.Equals(""))
+                analizeLogFile(logSelectionLbl.Text);
+            else
+                MessageBox.Show("Selezionare il file di log del sink e/o riempire i valori dei nodi e dei pacchetti!", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void analizeLogFile(string sinkPath)
+        {
+            logResultText.AppendText("Analisi del file di log del sink in corso...");
+            logResultText.AppendText(Environment.NewLine);
+            int[] packetRetransmissions = new int[Convert.ToInt32(numPackets.Text)];
+            int[] nodeRetransmissions = new int[Convert.ToInt32(numNodes.Text) + 1];
+            using (StreamReader sr = File.OpenText(sinkPath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] subStrings = line.Split('-');
+                    
+                    /*
+                     subStrings structure: 
+                        [0] = action made
+                        [1] = NODE_ID
+                        [2] = value associated with the action made
+                     */
+                    switch (subStrings[0])
+                    {
+                        case "RTXPKTTX":
+                            packetRetransmissions[Convert.ToInt32(subStrings[2])]++;
+                            break;
+                        case "RTXTOT":
+                            nodeRetransmissions[Convert.ToInt32(subStrings[1])] = Convert.ToInt32(subStrings[2]);
+                            break;
+                        default:
+                            break;
+                    }
+                    
+                }
+            }
+            try
+            {
+                for (int node = 0; node < Convert.ToInt32(numNodes.Text); node++)
+                {
+                    DialogResult dialogResult = MessageBox.Show("Premi Okay per aggiungere il file di log del prossimo nodo", "Avviso", MessageBoxButtons.OKCancel);
+                    if (dialogResult == DialogResult.OK)
+                    {
+                        string path = openedFilePath();
+
+                        logResultText.AppendText("Analisi del file di log del nodo " + (node + 1) + " in corso...");
+                        logResultText.AppendText(Environment.NewLine);
+
+                        using (StreamReader sr = File.OpenText(path))
+                        {
+                            string line;
+                            while ((line = sr.ReadLine()) != null)
+                            {
+                                string[] subStrings = line.Split('-');
+
+                                /*
+                                 subStrings structure: 
+                                    [0] = action made
+                                    [1] = NODE_ID
+                                    [2] = value associated with the action made
+                                 */
+                                switch (subStrings[0])
+                                {
+                                    case "RTXPKTTX":
+                                        packetRetransmissions[Convert.ToInt32(subStrings[2])]++;
+                                        break;
+                                    case "RTXTOT":
+                                        nodeRetransmissions[Convert.ToInt32(subStrings[1])] = Convert.ToInt32(subStrings[2]);
+                                        break;
+                                    default:
+                                        break;
+                                }
+
+                            }
+                        }
+                    }
+                }
+            } catch(Exception e)
+            {
+                MessageBox.Show("Nessun file selezionato!", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+
+            logResultText.AppendText("Creazione del file di output in: " + Path.GetDirectoryName(sinkPath) + @"\output.txt");
+            logResultText.AppendText(Environment.NewLine);
+
+            int i= 0,j = 0;
+            using (StreamWriter sw = File.CreateText(Path.GetDirectoryName(sinkPath)+@"\output.txt"))
+            {
+                while (i < packetRetransmissions.Length)
+                {
+                    sw.WriteLine("Packet " + i + " Total retransmissions: " + packetRetransmissions[i]);
+                    i++;
+                }
+                while (j < nodeRetransmissions.Length)
+                {
+                    sw.WriteLine("Node " + j + " Total retransmissions: " + nodeRetransmissions[j]);
+                    j++;
+                }
+            }
+            logResultText.AppendText("Analisi terminata.");
+            logResultText.AppendText(Environment.NewLine);
+        }
+
+        private string openedFilePath()
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    var filePath = openFileDialog.FileName;
+                    return filePath;
+                }
+            }
+            return null;
+        }
+
+        private void logSelectionBtn_Click(object sender, EventArgs e)
+        {
+            string path = openedFilePath();
+            if (path != null)
+                logSelectionLbl.Text = path;
+        }
+    }
+}
